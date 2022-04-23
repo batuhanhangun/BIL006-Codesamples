5e170c62ca72d0cc5d9567921d8a369617664650
Batuhan Hangün <38015481+batuhanhangun@users.noreply.github.com>
2022-04-23 11:08:11 +0300

    Update basics7_functions.py

--- basics/basics7_functions.py
+++ basics/basics7_functions.py
@@ -12,7 +12,7 @@ def capitalize_string(input_string: str):
     print("Input: " + input_string + "\nCapitalized input: " + input_string.upper())
 
 #Passing argument to a function with one parameter
-test_string = "küçük"
+test_string = 'küçük'
 capitalize_string(test_string)
 
 def print_user_info(input_name: str, input_surname: str, input_age: int):
@@ -20,24 +20,25 @@ def print_user_info(input_name: str, input_surname: str, input_age: int):
     print("Name: " + input_name + "\nSurname: " + input_surname + "\nAge: " + str(input_age))
     
 #Passing argument to a function with two parameters
-name = "Thomas"
-surname = "Shelby"
+name = 'Thomas'
+surname = 'Shelby'
 age = 25
 print_user_info(name, surname, age)
 
-#Syntax error. Argument order is important
 #print_user_info(age, name, surname)
+#Syntax error. Argument order is important!
 
-#Passing keyword arguments to a function with two parameters (argument order is NOT important)
-print_user_info(input_age=41, input_surname="White", input_name="Walter")
+#Passing keyword arguments to a function with parameters (argument order is NOT important)
+print_user_info(input_age = 41, input_surname = 'White', input_name = 'Walter')
 
-def student_info(student_name: str, student_surname: str, student_id: int, is_enrolled = False):
-    #Defining a function with default parameters
-    print("Name: " + student_name + "\nSurname: " + student_surname + "\nID: " + str(student_id) + "\nEnrolled: " + str(is_enrolled))
+def student_info(student_name: str, student_surname: str, student_id: int, is_enrolled = False, scholarship = int(0)):
+    #Defining a function with parameters which some of them have default arguments
+    print("Name: " + student_name + "\nSurname: " + student_surname + "\nID: " + str(student_id) + "\nEnrolled: " + str(is_enrolled) + "\nScholarship: " + str(scholarship))
 
 #Passing keyword arguments to a function with default arguments
-name = "Mika"
-surname = "Hakkinen"
+name = 'Mika'
+surname = 'Hakkinen'
 student_id = 321412512
 student_info(name, surname, student_id)
 student_info(name, surname, student_id, is_enrolled = True)
+student_info(name, surname, student_id, is_enrolled = True, scholarship = 40)
