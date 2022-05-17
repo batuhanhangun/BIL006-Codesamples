7a1651ba29d2abd00168b216c396146dceccb8ec
Batuhan <batuhanhangun@gmail.com>
2022-05-17 13:08:29 +0300

    Formatted

--- NumPy Examples/basics-3-random-number-generation.py
+++ NumPy Examples/basics-3-random-number-generation.py
@@ -1,10 +1,12 @@
 #Random number generation
 # Generating an array with random values inside an interval
 # Getting unique items in an array
+# Getting indices of unique elements
+# Getting occurence of each element
 
 import numpy
 
-#Generating an array with random values inside an interval
+#1)Generating an array with random values inside an interval
 max_value = 5
 num_rows = 4
 num_cols = 3
@@ -13,16 +15,18 @@ random_array = random_number_generator.integers(max_value + 1, size=(num_rows, n
 print('\n',random_array)
 print('\n')
 
-#Getting unique items in an array
+#2)Getting unique items in an array
 max_value = 4
 num_rows = 1
 num_cols = 6
 random_array2 = random_number_generator.integers(max_value + 1, size=(num_rows, num_cols))
 print('Random array: ', random_array2)
+
+#3)Getting indices of unique elements
 unique_values, unique_values_indices = numpy.unique(random_array2, return_index=True)
 print('Random array unique values: ', unique_values)
 print('Unique values indices: ', unique_values_indices)
 
-#Getting occurence of each element
+#4)Getting occurence of each element
 unique_values, occurrence_count = numpy.unique(random_array2, return_counts=True)
 print('Unique values occurence: ', occurrence_count)
